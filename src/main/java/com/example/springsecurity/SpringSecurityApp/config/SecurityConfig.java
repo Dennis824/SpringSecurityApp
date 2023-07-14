ddace6ef6f32ac6d7178a8f608cbe22d71d3859f
Dennis <57582915+Dennis824@users.noreply.github.com>
2023-07-14 16:39:14 -0600

    added authentication without custom authenticationProvider

--- src/main/java/com/example/springsecurity/SpringSecurityApp/config/SecurityConfig.java
+++ src/main/java/com/example/springsecurity/SpringSecurityApp/config/SecurityConfig.java
@@ -1,22 +1,32 @@
 package com.example.springsecurity.SpringSecurityApp.config;
 
-import com.example.springsecurity.SpringSecurityApp.security.AuthProviderImpl;
+import com.example.springsecurity.SpringSecurityApp.services.PersonDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig {
 
-    private final AuthProviderImpl authProvider;
+    private final PersonDetailsService personDetailsService;
 
     @Autowired
-    public SecurityConfig(AuthProviderImpl authProvider) {
-        this.authProvider = authProvider;
+    public SecurityConfig(PersonDetailsService personDetailsService) {
+        this.personDetailsService = personDetailsService;
     }
 
     // auth config
-    protected void configure(AuthenticationManagerBuilder auth) {
-        auth.authenticationProvider(authProvider);
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(personDetailsService);
+    }
+
+
+    //using basic spring authentication need to show how to encode the password. NoOpPasswordEncoder means no encoding
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 }
