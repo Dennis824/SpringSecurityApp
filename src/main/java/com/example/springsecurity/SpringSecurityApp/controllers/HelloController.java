ad2340b8dc90dd5ea09624a7a67ddcd2e368e37a
Dennis <57582915+Dennis824@users.noreply.github.com>
2023-07-20 13:14:48 -0600

    added reworked admin page

--- src/main/java/com/example/springsecurity/SpringSecurityApp/controllers/HelloController.java
+++ src/main/java/com/example/springsecurity/SpringSecurityApp/controllers/HelloController.java
@@ -2,14 +2,22 @@ package com.example.springsecurity.SpringSecurityApp.controllers;
 
 
 import com.example.springsecurity.SpringSecurityApp.security.PersonDetails;
+import com.example.springsecurity.SpringSecurityApp.services.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
+
+    private final AdminService adminService;
+
+    @Autowired
+    public HelloController(AdminService adminService) {
+        this.adminService = adminService;
+    }
 
     @GetMapping("/hello")
     public String sayHello(){
@@ -27,6 +35,7 @@ public class HelloController {
 
     @GetMapping("/admin")
     public String adminPage() {
+        adminService.doAdminStuff();
         return "admin";
     }
 }
